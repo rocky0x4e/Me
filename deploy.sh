5092ec4e426a38b1b33d85ef17fdcbf332863e62
Rocky <madfatcat88@gmail.com>
2025-10-19 11:19:21 -0500

    update phone num

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+pkill gunicorn -9
 
 python3 -m venv venv
 source ./venv/bin/activate
@@ -6,4 +7,5 @@ source ./venv/bin/activate
 pip install -r requirements.txt
 
 cd www
-gunicorn --pid ./pid --workers 4 --bind unix:app.sock wsgi:app
+gunicorn -D --log-file runtime.log \
+    --pid ./pid --workers 4 --bind unix:app.sock wsgi:app
