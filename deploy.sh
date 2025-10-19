2fc33a96f495ff06137ae4e9007fcacd6dcb2c8a
Rocky <madfatcat88@gmail.com>
2025-10-18 23:01:55 -0500

    gh action - try 3

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+pwd
+exit 0
 python3 -m venv venv
 source ./venv/bin/activate
 
 pip install -r requirements.txt
 
-echo $CWD
 
 gunicorn --pid ./pid --workers 4 --bind unix:app.sock wsgi:app
