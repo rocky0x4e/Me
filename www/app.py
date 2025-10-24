f274dee8b7d6a3db85a7c47bb3da410066d3576e
Rocky <madfatcat88@gmail.com>
2025-10-24 14:45:40 -0500

    add some links

--- www/app.py
+++ www/app.py
@@ -1,17 +1,25 @@
-import json
-import re
-import datetime
+
+import socket
+import netifaces
 from flask import Flask, render_template
-from lib.APIs.IncNodeMonitor import IncNodeMonitor
-from lib.Vnode import Vnode
 
 
 def create_app():
     app = Flask(__name__)
-    nodeMonitor = IncNodeMonitor()
 
     @app.route("/")
     def home():
-        return render_template("home.html")
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        myIp = "localhost"
+        gatewayIp = netifaces.gateways()["default"][netifaces.AF_INET][0]
+        try:
+            # Doesn't have to be reachable — just a valid IP
+            s.connect(("8.8.8.8", 80))
+            myIp = s.getsockname()[0]
+        finally:
+            s.close()
+
+        return render_template("home.html", gatewayAdmin=f"http://{gatewayIp}", adguardAdmin=f"http://{myIp}:8080")
 
     return app
