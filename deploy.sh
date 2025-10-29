496c02785d49282d48639dfbb8e1ad6f7ebde7e0
Rocky <madfatcat88@gmail.com>
2025-10-29 10:43:55 -0500

    remove hardcoded user name in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-SYSD_SERVICE=RockyWWW.service
+SERVICE_NAME=MyWWW
+SYSD_SERVICE_PATH=$HOME/.config/systemd/user
+SYSD_SERVICE_FILE=$SYSD_SERVICE_PATH/${SERVICE_NAME}.service
 WORKING_DIR=$(pwd -P)
+
 
 python3 -m venv venv
 source ./venv/bin/activate
 pip install -r requirements.txt
 
-systemctl --user stop $SYSD_SERVICE
+mkdir -p  $SYSD_SERVICE_PATH
+systemctl --user stop $SERVICE_NAME 2> /dev/null
 
-cat << EOF > /home/rocky/.config/systemd/user/$SYSD_SERVICE
+cat << EOF > $SYSD_SERVICE_FILE
 [Unit]
 Description = My python web server
 After = network.target
@@ -19,19 +23,19 @@ Type=notify
 WorkingDirectory = ${WORKING_DIR}/www
 ExecStart = ${WORKING_DIR}/venv/bin/gunicorn --pid ./pid --workers 4 --bind unix:${WORKING_DIR}/www/app.sock wsgi:app
 EOF
-cat << "EOF" >> /home/rocky/.config/systemd/user/$SYSD_SERVICE
+cat << "EOF" >> $SYSD_SERVICE_FILE
 ExecReload = /bin/kill -s HUP $MAINPID
 ExecStop = /bin/kill -s TERM $MAINPID
 EOF
-cat << EOF >> /home/rocky/.config/systemd/user/$SYSD_SERVICE
+cat << EOF >> $SYSD_SERVICE_FILE
 StandardOutput = journal
 StandardError = journal
-SyslogIdentifier = ${SYSD_SERVICE//.service/}
+SyslogIdentifier = $SERVICE_NAME
 
 [Install]
 WantedBy = default.target
 EOF
 
 systemctl --user daemon-reload
-systemctl --user enable $SYSD_SERVICE
-systemctl --user start $SYSD_SERVICE
+systemctl --user enable $SERVICE_NAME
+systemctl --user start $SERVICE_NAME
